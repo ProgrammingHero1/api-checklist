f09f06338108939b282e45ace686064b9d3112b0
Programming Hero <53802153+ProgrammingHero1@users.noreply.github.com>
2021-08-31 17:49:57 -0500

    Add files via upload

---
+++ api-checklist.js
@@ -0,0 +1,14 @@
+/*
+1. Object with property (string, boolean, number, object, array, method which will return a property value)
+2. template string. minimum 3 property will be dynamic (nested object property, array second element, another property)
+3.1: no parameter arrow function. return 89
+3.2: single parameter arrow function. will divide input by 7
+3.3: arrow function with two parameters.
+3.4 multi-line arrow function. two parameter.
+.
+4. map divide each element by 7
+5. map, forEach, filter, find
+6. desctructuring on object and array
+.
+
+*/
---
+++ api.js
@@ -0,0 +1,60 @@
+const loadSingleUser = () => {
+    fetch('https://randomuser.me/api/')
+        .then(res => res.json())
+        .then(data => displaySingleUser(data.results[0]))
+}
+loadSingleUser();
+
+const displaySingleUser = user => {
+    console.log(user);
+}
+
+// meal db
+const toggleSpinner = displayStyle => {
+    document.getElementById('spinner').style.display = displayStyle;
+}
+const toggleSearchResult = displayStyle => {
+    document.getElementById('meals').style.display = displayStyle;
+}
+const searchMeal = () => {
+    const searchText = document.getElementById('search-field').value;
+
+    // display spinner
+    toggleSpinner('block');
+    toggleSearchResult('none');
+    loadMeals(searchText);
+    document.getElementById('search-field').value = '';
+}
+
+const loadMeals = searchText => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayMeals(data.meals))
+}
+
+const displayMeals = meals => {
+    const container = document.getElementById('meals');
+    container.textContent = '';
+    if (!meals) {
+
+    }
+    meals?.forEach(meal => {
+        console.log(meal);
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <h1>${meal.strMeal}</h1>
+            <p>${meal.strIngredient18 ? meal.strIngredient18 : ''}</p>
+            <button onclick="loadMealDetail('${meal.strMeal}')">click me</button>
+        `;
+        container.appendChild(div);
+    });
+    toggleSpinner('none');
+    toggleSearchResult('block');
+}
+
+loadMeals('fish');
+
+const loadMealDetail = mealName => {
+    console.log(mealName);
+}
